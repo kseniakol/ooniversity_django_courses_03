58f938a9a55efa049d4330afce5f6b363e04c32c
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-12-24 00:20:53 +0200

    feedback/forms.py: from_email = forms.EmailField(required=False)

--- feedbacks/forms.py
+++ feedbacks/forms.py
@@ -4,5 +4,7 @@ from feedbacks.models import Feedback
 
 # Create your views here.
 class FeedbackForm(forms.ModelForm):
+    from_email = forms.EmailField(required=False)
+
     class Meta:
         model = Feedback
